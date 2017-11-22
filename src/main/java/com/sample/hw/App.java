cb278aa77d3863aec92f9a370c3ee3207b105019
Pravinkumar <abhilash.pravinkumar@cgi.com>
2017-11-22 12:58:32 -0500

    First commit

---
+++ src/main/java/com/sample/hw/App.java
@@ -0,0 +1,15 @@
+package com.sample.hw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String sayHello(){
+        return "Hello World";
+    }
+    public String sayHelloWithName(String name){
+        return "Hello " + name;
+    }
+}
---
+++ src/test/java/com/sample/hw/AppTest.java
@@ -0,0 +1,28 @@
+package com.sample.hw;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+   @Test
+    public void shouldSayHello(){
+        App greeter = new App();
+        String hello = greeter.sayHello();
+        assertTrue(hello.equals("Hello World"));
+
+   }
+   @Test
+   public void shouldSayHelloName(){
+        App greeter = new App();
+        String hello = greeter.sayHelloWithName("Abhi");
+        assertTrue(hello.equals("Hello Abhi"));
+
+   }
+}
